817d1ad364bd652ad8027d08dec880ffb20982c8
iskrabozhkova <52824905+iskrabozhkova@users.noreply.github.com>
2023-05-09 18:58:24 +0300

    add-images-to-database

--- src/main/java/com/example/demo/services/ImageService.java
+++ src/main/java/com/example/demo/services/ImageService.java
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,62 +18,68 @@ public class ImageService {
     private ImageRepository imageRepository;
     private LabelRepository labelRepository;
     private ImaggaAPI imaggaAPI;
-    public ImageService(){}
+
+    public ImageService() {
+    }
+
     @Autowired
-    public ImageService(ImageRepository imageRepository, ImaggaAPI imaggaAPI, LabelRepository labelRepository){
+    public ImageService(ImageRepository imageRepository, ImaggaAPI imaggaAPI, LabelRepository labelRepository) {
         this.imageRepository = imageRepository;
         this.labelRepository = labelRepository;
         this.imaggaAPI = imaggaAPI;
     }
 
-public String uploadImage(Image image) {
-
-    String imageUrl = image.getUrl();
-    Image foundImage = getImageByUrl(imageUrl);
-    //uploaded image not exists in database
-    if (foundImage == null) {
+    public String categorizeImage(Image image){
+        String imageUrl = image.getUrl();
         String jsonResponse = imaggaAPI.categorizeImage(imageUrl);
 
         List<Label> labels = new ArrayList<>();
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode responseJson = objectMapper.readTree(jsonResponse);
-            JsonNode resultJson = responseJson.get("result");
-            JsonNode tagsJson = resultJson.get("tags");
-            for (JsonNode tagNode : tagsJson) {
-                JsonNode tagJson = tagNode.get("tag");
-                String labelName = tagJson.get("en").asText();
+            try {
+                ObjectMapper objectMapper = new ObjectMapper();
+                JsonNode responseJson = objectMapper.readTree(jsonResponse);
+                JsonNode resultJson = responseJson.get("result");
+                JsonNode tagsJson = resultJson.get("tags");
+                for (JsonNode tagNode : tagsJson) {
+                    JsonNode tagJson = tagNode.get("tag");
+                    String labelName = tagJson.get("en").asText();
 
-                Label label = new Label();
-                label.setName(labelName);
-                labels.add(label);
+                    Label label = new Label();
+                    label.setName(labelName);
+                    labels.add(label);
+                }
+                image.setLabels(labels);
+                //save image to the database
+                imageRepository.save(image);
+            } catch (JsonProcessingException | NullPointerException e) {
+                e.printStackTrace();
             }
-            image.setLabels(labels);
-            //save image to the database
-            imageRepository.save(image);
-        } catch (JsonProcessingException | NullPointerException e) {
-            e.printStackTrace();
-        }
         return jsonResponse;
-    } else {
-        //get image tags from the database
-        List<Label> foundImageLabels = foundImage.getLabels();
-
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.findAndRegisterModules();
-        String json;
-        try {
-            json = mapper.writeValueAsString(foundImageLabels);
-            return json;
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
     }
-    return "";
-}
-    public Image getImageByUrl(String url){
+    public String getImageLabels(Image image){
+        List<Label> foundImageLabels = image.getLabels();
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.findAndRegisterModules();
+            String json;
+            try {
+                json = mapper.writeValueAsString(foundImageLabels);
+                return json;
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+        return "";
+    }
+    public String uploadImage(Image image) {
+        String imageUrl = image.getUrl();
+        Image foundImage = getImageByUrl(imageUrl);
+        //uploaded image not exists in database
+        if (foundImage == null) {
+            return categorizeImage(image);
+        }
+        return getImageLabels(foundImage);
+    }
+    public Image getImageByUrl(String url) {
         Image image = imageRepository.findByUrl(url);
-        if(image != null){
+        if (image != null) {
             return image;
         }
         return null;
