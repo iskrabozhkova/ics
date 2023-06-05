7eefd20235168bd27be2ba630010cac08efada91
iskrabozhkova <52824905+iskrabozhkova@users.noreply.github.com>
2023-06-05 17:33:58 +0300

    style changes

--- client/src/app/service/image.service.ts
+++ client/src/app/service/image.service.ts
@@ -7,36 +7,37 @@ import { Image } from '../interfaces/image';
   providedIn: 'root',
 })
 export class ImageService {
-
   private apiUrl = 'http://localhost:8080/api/images';
 
-  constructor(private http : HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-  getImages() : Observable<Image[]> { 
+  getApiUrl(): string {
+    return this.apiUrl;
+  }
+
+  getImages(): Observable<Image[]> {
     return this.http.get<Image[]>(this.apiUrl);
   }
 
-  getApiUrl() : string {
-    return this.apiUrl;
-  }
-  
-  uploadImage(image : Image) : Observable<Image> {
+  uploadImage(image: Image): Observable<Image> {
     const headers = new HttpHeaders().set('Content-Type', 'application/json');
 
     return this.http.post<Image>(this.apiUrl, image, { headers: headers });
   }
 
-  getImageById(imageId : string): Observable<Image> {
-    return this.http.get<Image>(`${ this.apiUrl }/${ imageId }`);
+  getImageById(imageId: string): Observable<Image> {
+    return this.http.get<Image>(`${ this.apiUrl }/${ imageId}`);
   }
 
-  searchByLabels(labels : string) : Observable<Image[]> {
-    const url = `${ this.apiUrl }?labels=${ labels }`;
+  searchByLabels(labels: string): Observable<Image[]> {
+    const url = `${this.apiUrl}?labels=${labels}`;
 
     return this.http.get<Image[]>(url);
   }
 
-  deleteImage(imageId? : number) : Observable<string> {
-    return this.http.delete(`${ this.apiUrl }/${ imageId }`, { responseType: 'text' });
+  deleteImage(imageId?: number): Observable<string> {
+    return this.http.delete(`${this.apiUrl}/${imageId}`, {
+      responseType: 'text',
+    });
   }
 }
